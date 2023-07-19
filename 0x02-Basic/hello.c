14cbf25a6c017bd6f94dc3a296f9f6ad51397c32
EkenoSamson <samsonekeno98@gmail.com>
2023-07-19 02:28:50 -0700

    hello Wednesday

---
+++ 0x02-Basic/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * Description: display hello world.
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("Hello world!\n");
+	return (0);
+}
